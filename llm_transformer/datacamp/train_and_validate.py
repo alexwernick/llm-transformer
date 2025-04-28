2738991fe91030cb03067fd9d45615bb758dc529
Alex Wernick <alex.j.wernick@gmail.com>
2025-04-28 18:22:50 +0100

    off debug

--- llm_transformer/datacamp/train_and_validate.py
+++ llm_transformer/datacamp/train_and_validate.py
@@ -23,12 +23,12 @@ def main():
     # tokenizer but there must be a better way
     dropout = 0.1  # Regularizes the model to prevent overfitting
     learning_rate = 0.0001  # Impacts convergence speed and stability
-    batch_size = 5  # noqa: E501 Larger batch sizes improve learning stability but require more memory
+    batch_size = 50  # noqa: E501 Larger batch sizes improve learning stability but require more memory
     epochs = 3  # 100
 
     # src_data, tgt_data = generate_data(src_vocab_size, tgt_vocab_size, max_seq_length)
     language_pair = "en-es"
-    datasets = load_opus_dataset(language_pair, debug_mode=True, debug_samples=10)
+    datasets = load_opus_dataset(language_pair, debug_mode=False, debug_samples=10)
 
     tokenizer = AutoTokenizer.from_pretrained(f"Helsinki-NLP/opus-mt-{language_pair}")
     criterion = nn.CrossEntropyLoss(ignore_index=tokenizer.pad_token_id).to(device)
